c0cdbdb615fe8e735b12b55b70bb81b787ea4708
xZero707 <aleksandar@puharic.com>
2025-03-29 17:15:07 +0100

    Update tests accordingly to the changes

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,19 +4,22 @@ err() {
     printf "%s\n" "$*" >&2
 }
 
-assert_secret_exists_and_matches_normalized() {
+assert_secret_exists_and_matches() {
     local secret=${1:?}
     local expected_value="${2:?}"
     local additional_message="${3:-}"
-    local normalizedSecretFile="${NORMALIZED_SECRETS_PATH}/${secret}"
+    local exported_secret_file="${SECRETS_EXPORT_PATH}/${secret}"
 
-    if [ ! -f "${normalizedSecretFile}" ]; then
+    if [ ! -f "${exported_secret_file}" ]; then
         err "Failed asserting that secret ${secret} exists"
         return 1
     fi
 
-    if [ "$(cat "${normalizedSecretFile}")" != "${expected_value}" ]; then
-        err "Failed asserting that secret ${secret} matches the expected value"
+    local actual_value
+    actual_value=$(cat "${exported_secret_file}")
+    if [ "${actual_value}" != "${expected_value}" ]; then
+        err "Failed asserting that secret ${secret} matches the expected value."
+        err "$(printf "Expected != Actual: '%s' != '%s'" "${expected_value}" "${actual_value}")"
 
         if [ -n "${additional_message}" ]; then
             err "${additional_message}"
@@ -46,11 +49,11 @@ assert_environment_variable_exists_and_matches_value() {
     return 0
 }
 
-test_primary_functionality() {
+test_primary_functionality_normalized_secrets() {
     # Create temporary directory names
 
     # Create secret file in the normalized secrets directory
-    echo "test_secret value_0" >"${NORMALIZED_SECRETS_PATH}/TEST_SECRET0"
+    echo "test_secret value_0" >"${SECRETS_EXPORT_PATH}/TEST_SECRET0"
 
     # Create a secret files
     echo "should not be exported" >"${SECRETS_PATH}/test_secret0"
@@ -62,15 +65,37 @@ test_primary_functionality() {
     # Run the script
     ./src/init-docker-secrets-run.sh
 
-    assert_secret_exists_and_matches_normalized "TEST_SECRET0" "test_secret value_0" "- Secret was probably overwritten" || return 1
-    assert_secret_exists_and_matches_normalized "TEST_SECRET1" "test_secret value_1" || return 1
-    assert_secret_exists_and_matches_normalized "TEST_SECRET2" "test_secret value_2" || return 1
-    assert_secret_exists_and_matches_normalized "TEST_SECRET3" "test_secret value_3" || return 1
-    assert_secret_exists_and_matches_normalized "TEST_SECRET4" "test_secret value_4" || return 1
+    assert_secret_exists_and_matches "TEST_SECRET0" "test_secret value_0" "- Secret was probably overwritten" || return 1
+    assert_secret_exists_and_matches "TEST_SECRET1" "test_secret value_1" || return 1
+    assert_secret_exists_and_matches "TEST_SECRET2" "test_secret value_2" || return 1
+    assert_secret_exists_and_matches "TEST_SECRET3" "test_secret value_3" || return 1
+    assert_secret_exists_and_matches "TEST_SECRET4" "test_secret value_4" || return 1
+}
+
+test_primary_functionality_non_normalized_secrets() {
+    # Create temporary directory names
+
+    # Create a secret files
+    echo "test_secret value_0" >"${SECRETS_PATH}/test_secret0"
+    echo "test_secret value_1" >"${SECRETS_PATH}/test_secret1"
+    echo "test_secret value_2" >"${SECRETS_PATH}/test_secret2"
+    echo "test_secret value_3" >"${SECRETS_PATH}/test_secret3"
+    echo "test_secret value_4" >"${SECRETS_PATH}/TEST_SECRET4" # Should be able to handle this too
+
+    export NORMALIZE_SECRET_NAMES=0
+
+    # Run the script
+    ./src/init-docker-secrets-run.sh
+
+    assert_secret_exists_and_matches "test_secret0" "test_secret value_0" "- Secret was probably overwritten" || return 1
+    assert_secret_exists_and_matches "test_secret1" "test_secret value_1" || return 1
+    assert_secret_exists_and_matches "test_secret2" "test_secret value_2" || return 1
+    assert_secret_exists_and_matches "test_secret3" "test_secret value_3" || return 1
+    assert_secret_exists_and_matches "TEST_SECRET4" "test_secret value_4" || return 1
 }
 
 test_load_env() {
-    source src/load-env.sh "${NORMALIZED_SECRETS_PATH}"
+    source src/load-env.sh "${SECRETS_EXPORT_PATH}"
 
     assert_environment_variable_exists_and_matches_value TEST_SECRET0 "test_secret value_0" || return 1
     assert_environment_variable_exists_and_matches_value TEST_SECRET1 "test_secret value_1" || return 1
@@ -84,19 +109,19 @@ main() {
     # Create temporary directory names
 
     SECRETS_PATH="$(mktemp -d)"
-    NORMALIZED_SECRETS_PATH="$(mktemp -d)"
+    SECRETS_EXPORT_PATH="$(mktemp -d)"
 
-    export SECRETS_PATH NORMALIZED_SECRETS_PATH
+    export SECRETS_PATH SECRETS_EXPORT_PATH
 
-    trap 'rm -rf "${SECRETS_PATH}" "${NORMALIZED_SECRETS_PATH}"' EXIT
+    trap 'rm -rf "${SECRETS_PATH}" "${SECRETS_EXPORT_PATH}"' EXIT
 
     local failed_tests=0
 
-    if ! test_primary_functionality; then
-        echo "- Test FAIL: test_primary_functionality"
+    if ! test_primary_functionality_normalized_secrets; then
+        echo "- Test FAIL: test_primary_functionality_normalized_secrets"
         ((failed_tests++))
     else
-        echo "- Test OK: test_primary_functionality"
+        echo "- Test OK: test_primary_functionality_normalized_secrets"
     fi
 
     if ! test_load_env; then
@@ -104,6 +129,20 @@ main() {
         ((failed_tests++))
     else
         echo "- Test OK: test_load_env"
+    fi
+
+    # Regenerate temporary directories
+    rm -rf "${SECRETS_PATH}" "${SECRETS_EXPORT_PATH}"
+    SECRETS_PATH="$(mktemp -d)"
+    SECRETS_EXPORT_PATH="$(mktemp -d)"
+    export SECRETS_PATH SECRETS_EXPORT_PATH
+    trap 'rm -rf "${SECRETS_PATH}" "${SECRETS_EXPORT_PATH}"' EXIT
+
+    if ! test_primary_functionality_non_normalized_secrets; then
+        echo "- Test FAIL: test_primary_functionality_non_normalized_secrets"
+        ((failed_tests++))
+    else
+        echo "- Test OK: test_primary_functionality_non_normalized_secrets"
     fi
 
     if [ "${failed_tests}" -gt 0 ]; then
